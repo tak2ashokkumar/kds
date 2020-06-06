5a7c7dea33eec1d85b9e21e70bb29b17bfa3dadf
Ashok Kumar <tak2ashokkumar@gmail.com>
2020-06-06 21:54:01 +0530

    removed un necessary modules

--- src/app/app-promotions/app-promotions-routing.module.ts
+++ src/app/app-promotions/app-promotions-routing.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppPromotionsComponent } from './app-promotions.component';
-import { BillPaymentsComponent } from './bill-payments/bill-payments.component';
-import { LoanProvidersComponent } from './loan-providers/loan-providers.component';
-import { UtilitiesComponent } from './utilities/utilities.component';
 import { ReferralCodesComponent } from './referral-codes/referral-codes.component';
 
 
 const routes: Routes = [
-  { 
+  {
     path: '', component: AppPromotionsComponent,
-    children:[
-      { path: 'bill_payments', component: BillPaymentsComponent },
-      { path: 'loan_providers', component: LoanProvidersComponent },
+    children: [
       { path: 'shopping_vendors', component: ReferralCodesComponent },
       { path: 'referrals', component: ReferralCodesComponent },
-      { path: 'utilities', component: UtilitiesComponent },
       { path: '', redirectTo: 'referrals', pathMatch: 'full' },
-    ] 
+    ]
   },
 ];
 
--- src/app/app-promotions/app-promotions.module.ts
+++ src/app/app-promotions/app-promotions.module.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppPromotionsRoutingModule } from './app-promotions-routing.module';
 import { AppPromotionsComponent } from './app-promotions.component';
 import { AppHeaderComponent } from '../app-header/app-header.component';
-import { BillPaymentsComponent } from './bill-payments/bill-payments.component';
-import { LoanProvidersComponent } from './loan-providers/loan-providers.component';
 import { ReferralCodesComponent } from './referral-codes/referral-codes.component';
-import { UtilitiesComponent } from './utilities/utilities.component';
 import { AppCoreModuleModule } from '../app-core-module/app-core-module.module';
 
 
@@ -14,10 +11,7 @@ import { AppCoreModuleModule } from '../app-core-module/app-core-module.module';
   declarations: [
     AppHeaderComponent,
     AppPromotionsComponent,
-    BillPaymentsComponent,
-    LoanProvidersComponent,
     ReferralCodesComponent,
-    UtilitiesComponent
   ],
   imports: [
     AppCoreModuleModule,
